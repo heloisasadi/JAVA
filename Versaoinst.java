2daa8da0f25d4353c24b8d212aa91878f4338b72
Heloisa <bheloisa437@gmail.com>
2025-09-21 11:03:27 -0300

    Create Versaoinst.java

---
+++ Versaoinst.java
@@ -0,0 +1,8 @@
+public class versao {
+    public static void main(String[] args){
+
+        String versao = System.getProperty("java.version");
+        System.out.println("A Versão do Java instalada é: " + versao);
+
+    }
+}
